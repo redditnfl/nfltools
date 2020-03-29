83cf48f22337eac35d932802da02c105b3c74e6b
Jonas Häggqvist <rasher@rasher.dk>
2020-03-29 03:59:10 +0200

    Add 2020 draft

--- redditnfl/nfltools/draft.py
+++ redditnfl/nfltools/draft.py
@@ -98,77 +98,102 @@ drafts = parsedrafts("""
 5: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c
 6: 1,2,3,4,5,6,7,8,9,10,11,12,13,14X,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c,36c,37c,38c,39c,40c,41c,42c
 7: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c,36c,37c,38c,39c,40c
+---
+2020
+1: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
+2: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
+3: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c,36c,37c,38c,39c,40c,41c,42c
+4: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c,36c,37c,38c,39c,40c
+5: 1,2,3,4,5,6,7,8X,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c
+6: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c
+7: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33c,34c,35c,36c,37c,38c,39c,40c,41c
 """)
 
+if __name__ == "__main__":
+    assert overall(2017, 1, 1) == 1
+    assert overall(2017, 2, 1) == 33
+    assert overall(2017, 3, 1) == 65
+    assert overall(2017, 4, 1) == 108
+    assert overall(2017, 4, 13) == 119
+    assert overall(2017, 4, 35) == 141
+    assert overall(2017, 5, 1) == 145
+    assert overall(2017, 5, 41) == 184
+    assert overall(2017, 6, 1) == 185
+    assert overall(2017, 7, 1) == 219
+    assert overall(2017, 6, 26) == 210
+    assert overall(2017, 6, 28) == 211
+    assert overall(2017, 6, 35) == 218
 
-assert overall(2017, 1, 1) == 1
-assert overall(2017, 2, 1) == 33
-assert overall(2017, 3, 1) == 65
-assert overall(2017, 4, 1) == 108
-assert overall(2017, 4, 13) == 119
-assert overall(2017, 4, 35) == 141
-assert overall(2017, 5, 1) == 145
-assert overall(2017, 5, 41) == 184
-assert overall(2017, 6, 1) == 185
-assert overall(2017, 7, 1) == 219
-assert overall(2017, 6, 26) == 210
-assert overall(2017, 6, 28) == 211
-assert overall(2017, 6, 35) == 218
+    assert pick_type(2017, 5, 0) == UNKNOWN
+    assert pick_type(2017, 5, 1) == None
+    assert pick_type(2017, 5, 25) == None
+    assert pick_type(2017, 5, 26) == FORFEITED
+    assert pick_type(2017, 5, 27) == None
+    assert pick_type(2017, 5, 32) == None
+    assert pick_type(2017, 5, 33) == COMP
+    assert pick_type(2017, 5, 41) == COMP
+    assert pick_type(2017, 5, 42) == UNKNOWN
 
-assert pick_type(2017, 5, 0) == UNKNOWN
-assert pick_type(2017, 5, 1) == None
-assert pick_type(2017, 5, 25) == None
-assert pick_type(2017, 5, 26) == FORFEITED
-assert pick_type(2017, 5, 27) == None
-assert pick_type(2017, 5, 32) == None
-assert pick_type(2017, 5, 33) == COMP
-assert pick_type(2017, 5, 41) == COMP
-assert pick_type(2017, 5, 42) == UNKNOWN
+    assert round_pick(2017, 1) == (1,1)
+    assert round_pick(2017, 32) == (1,32)
 
-assert round_pick(2017, 1) == (1,1)
-assert round_pick(2017, 32) == (1,32)
+    assert round_pick(2017, 33) == (2,1)
+    assert round_pick(2017, 64) == (2,32)
 
-assert round_pick(2017, 33) == (2,1)
-assert round_pick(2017, 64) == (2,32)
+    assert round_pick(2017, 65) == (3,1)
+    assert round_pick(2017, 96) == (3,32)
+    assert round_pick(2017, 97) == (3,33)
+    assert round_pick(2017, 107) == (3,43)
 
-assert round_pick(2017, 65) == (3,1)
-assert round_pick(2017, 96) == (3,32)
-assert round_pick(2017, 97) == (3,33)
-assert round_pick(2017, 107) == (3,43)
+    assert round_pick(2017, 108) == (4,1)
+    assert round_pick(2017, 117) == (4,10)
+    assert round_pick(2017, 118) == (4,12)
+    assert round_pick(2017, 128) == (4,22)
+    assert round_pick(2017, 129) == (4,23)
+    assert round_pick(2017, 137) == (4,31)
+    assert round_pick(2017, 144) == (4,38)
 
-assert round_pick(2017, 108) == (4,1)
-assert round_pick(2017, 117) == (4,10)
-assert round_pick(2017, 118) == (4,12)
-assert round_pick(2017, 128) == (4,22)
-assert round_pick(2017, 129) == (4,23)
-assert round_pick(2017, 137) == (4,31)
-assert round_pick(2017, 144) == (4,38)
+    assert round_pick(2017, 145) == (5,1)
+    assert round_pick(2017, 169) == (5,25)
+    assert round_pick(2017, 170) == (5,27)
+    assert round_pick(2017, 175) == (5,32)
+    assert round_pick(2017, 176) == (5,33)
+    assert round_pick(2017, 184) == (5,41)
 
-assert round_pick(2017, 145) == (5,1)
-assert round_pick(2017, 169) == (5,25)
-assert round_pick(2017, 170) == (5,27)
-assert round_pick(2017, 175) == (5,32)
-assert round_pick(2017, 176) == (5,33)
-assert round_pick(2017, 184) == (5,41)
+    assert round_pick(2017, 185) == (6,1)
+    assert round_pick(2017, 210) == (6,26)
+    assert round_pick(2017, 211) == (6,28)
+    assert round_pick(2017, 215) == (6,32)
+    assert round_pick(2017, 216) == (6,33)
+    assert round_pick(2017, 218) == (6,35)
 
-assert round_pick(2017, 185) == (6,1)
-assert round_pick(2017, 210) == (6,26)
-assert round_pick(2017, 211) == (6,28)
-assert round_pick(2017, 215) == (6,32)
-assert round_pick(2017, 216) == (6,33)
-assert round_pick(2017, 218) == (6,35)
+    assert round_pick(2017, 219) == (7,1)
+    assert round_pick(2017, 250) == (7,32)
+    assert round_pick(2017, 251) == (7,33)
+    assert round_pick(2017, 253) == (7,35)
 
-assert round_pick(2017, 219) == (7,1)
-assert round_pick(2017, 250) == (7,32)
-assert round_pick(2017, 251) == (7,33)
-assert round_pick(2017, 253) == (7,35)
+    assert overall(2019, 7, 40) == 254
+    assert pick_type(2019, 3, 7) == FORFEITED
+    assert pick_type(2019, 3, 33) == COMP
+    assert pick_type(2019, 3, 40) == UNKNOWN
+    assert pick_type(2019, 4, 1) == None
 
-assert overall(2019, 7, 40) == 254
-assert pick_type(2019, 3, 7) == FORFEITED
-assert pick_type(2019, 3, 33) == COMP
-assert pick_type(2019, 3, 40) == UNKNOWN
-assert pick_type(2019, 4, 1) == None
+    assert pick_type(2020, 5, 8) == FORFEITED
+    assert len(drafts[2020][0]) == 32
+    assert len(drafts[2020][1]) == 32
+    assert len(drafts[2020][2]) == 42
+    assert len(drafts[2020][3]) == 40
+    assert len(drafts[2020][4]) == 34
+    assert len(drafts[2020][5]) == 35
+    assert len(drafts[2020][6]) == 41
+    assert overall(2020, 1, 1) == 1
+    assert overall(2020, 2, 1) == 33
+    assert overall(2020, 3, 1) == 65
+    assert overall(2020, 4, 1) == 107
+    assert overall(2020, 5, 1) == 147
+    assert overall(2020, 6, 1) == 180
+    assert overall(2020, 7, 1) == 215
 
-for year in drafts:
-    assert len(drafts[year]) == 7
-    assert sum([len(rd) for rd in drafts[year]]) == 256
+    for year in drafts:
+        assert len(drafts[year]) == 7
+        assert sum([len(rd) for rd in drafts[year]]) == 256
